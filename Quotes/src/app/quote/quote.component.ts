d60edf4bc5345d14b80bfe7dfadeb039438266ed
peter max <petermax2004@gmail.com>
2019-06-03 11:38:04 +0300

    Move the codes to the componenet

--- Quotes/src/app/quote/quote.component.ts
+++ Quotes/src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes = [
+    new Quote(1,'Early to bed Early to rise'),
+    new Quote(2,'Hurry Hurry has no Blessings'),
+  ]
 
   constructor() { }
 
