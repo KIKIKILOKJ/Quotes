e732b0f3e81fec1b74190c0bf541dfe16b1e7da7
peter max <petermax2004@gmail.com>
2019-06-03 16:27:04 +0300

    Add the args on quotes.ts

--- Quotes/src/app/quote.ts
+++ Quotes/src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   public showDescription:boolean;
-  constructor(public id:number, public name:string, public description:string, public submitDate:Date){
+  constructor(public id:number, public name:string, public description:string, public submitDate:Date, public upvotes:number, public downvotes:number, public highest:boolean){
     this.showDescription =false
 
   }
