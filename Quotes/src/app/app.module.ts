8b09c87ba97d6408d1a58e929d6027a59fefbfd2
peter max <petermax2004@gmail.com>
2019-06-03 11:11:21 +0300

    Add the angularjs files and folders

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
-import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    QuotesFormComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Quotes/src/app/quotes.ts
+++
@@ -1,5 +0,0 @@
-export class Quotes {
-  constructor(public id:number, public name:string, public submitDate:Date){
-    
-  }
-}
