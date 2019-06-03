11997e2493f3f53d0b79054fa8e12a61b769c0fc
peter max <petermax2004@gmail.com>
2019-06-03 14:46:03 +0300

    Add the form component

--- Quotes/src/app/app.module.ts
+++ Quotes/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { DateSubmitPipe } from './date-submit.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    DateSubmitPipe
+    DateSubmitPipe,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule
