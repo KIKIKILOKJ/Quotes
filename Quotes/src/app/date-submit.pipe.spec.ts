04c052087a2a78a651c68f49f8f2c0bd8f26024f
peter max <petermax2004@gmail.com>
2019-06-04 13:59:16 +0300

    Auto-generated commit

--- Quotes/src/app/date-submit.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateSubmitPipe } from './date-submit.pipe';
-
-describe('DateSubmitPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateSubmitPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- Quotes/src/app/quote.ts
+++
@@ -1,7 +0,0 @@
-export class Quote {
-  public showDescription:boolean;
-  constructor(public id:number, public name:string, public description:string, public submitDate:Date){
-    this.showDescription =false
-
-  }
-}
